2c3527e401210a09160e14bfeb31848621c0255b
Martin Javier Echenique <martin43108@gmail.com>
2023-08-09 15:00:36 -0300

    finalizacion product detail

--- app.js
+++ app.js
@@ -23,7 +23,16 @@ app.get('/register', (req,res) => {
     res.sendFile(path.join(__dirname, './views/register.html'))
 })
 app.get('/airForce', (req,res) => {
-    res.sendFile(path.join(__dirname, './views/productos/airForce.html'))
+    res.sendFile(path.join(__dirname, './views/airForce.html'))
+})
+app.get('/airMax', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/airMax.html'))
+})
+app.get('/jordan', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/jordan.html'))
+})
+app.get('/react', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/react.html'))
 })
 app.get('/productCart', (req,res) => {
     res.sendFile(path.join(__dirname, './views/productCart.html'))
