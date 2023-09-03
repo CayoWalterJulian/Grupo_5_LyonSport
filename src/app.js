9da2e33c95a36479585700e37ee5b22ef11a2bfe
CayoWalterJulian <catojulian15@gmail.com>
2023-09-03 20:38:10 -0300

    se creo el archivo users.json

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const {editoradd}= require("./routes/aditoradd");
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 app.use (express.static("public"))
 app.set("view engine" , "ejs")
 app.set("views", "./src/views")
