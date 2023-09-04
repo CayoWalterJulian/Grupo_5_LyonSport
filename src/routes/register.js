f295ddbf1d0b5408b615f374b63e466c9bc9f869
CayoWalterJulian <catojulian15@gmail.com>
2023-09-04 19:59:41 -0300

    se modifico ruta de deseos y se agrego codigo de rutas de register y views

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const registerController = require("../controller/register");
+const multer = require("multer")
 
-
+const multerDiskStorage =multer.diskStorage({
+    destination: (req,res,callback) => {
+        const folder = path.join(__dirname, "../public/imageProfile");
+        callback(null, folder);
+    },
+    filename: (req,res,callback) => {
+        const imageName = Date.now() + path.extname(file.originalname);
+        callback(null, imageName);
+    }
+})
 
 const register = express.Router();
 
