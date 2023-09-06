6de39dc149b1f06d850eb232511e0f7e06c6cd9e
Martin Javier Echenique <martin43108@gmail.com>
2023-09-06 20:07:36 -0300

    cree la view de products

--- src/controller/products.js
+++ src/controller/products.js
@@ -3,12 +3,19 @@ const products = require('../data/products.json')
 
 const productDetail = (req,res) => {
     const { id } = req.params;
-    const product = products.find(p => p.id == id)
+    const product = products.find(p => p.id === id)
 
     res.render('productDetail', {product})
 }
  
 
+const productosTotal = (req, res) => {
+
+    res.render('products', { products })
+
+}
+
 module.exports= {
-    productDetail
+    productDetail,
+    productosTotal
 }
