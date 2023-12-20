2288b9b83bae2d5be00dc9092740a03e429ba476
julian <catojulian15@gmail.com>
2023-12-20 18:31:44 -0300

    se realizaron cambios

--- src/routes/users.js
+++ src/routes/users.js
@@ -24,7 +24,7 @@ users.get('/logout/', usersController.logout)
 users.get('/deleteAccount', usersController.pageDeleteAccount)
 users.post('/deleteAccount', usersController.deleteAccount)
 
-users.get('/users/api', dataUsersApi)
-users.post('/users/api', dataUsersApi2)
+users.get('/users/api', usersController.dataUsersApi)
+users.post('/users/api', usersController.dataUsersApi2)
 
 module.exports = { users }
