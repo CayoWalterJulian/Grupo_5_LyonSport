150ef10c7ce2879cc1f0190624aea8dd5f086aa0
Martin Javier Echenique <martin43108@gmail.com>
2023-12-21 13:23:29 -0300

    agregue dependencias

--- src/app.js
+++ src/app.js
@@ -8,12 +8,15 @@ const methodOverride = require("method-override");
 const morgan = require('morgan');
 const session = require("express-session");
 const cookies = require('cookie-parser')
+const cors = require('cors')
 
 
 
 const app = express();
 const PORT = 3000;
 
+
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }));
 
