e19c64ea02cbc4c43d61d1641bd99c75ab18f13f
julian <catojulian15@gmail.com>
2024-01-09 12:36:04 -0300

    se arreglaron pequeños detalles

--- public/js/validacionesL.js
+++ public/js/validacionesL.js
@@ -1,28 +1,24 @@
 window.addEventListener("load", function(){
     let formulario = document.querySelector("form.login")
     let ulErrores = document.querySelector("div.errores ul")
-    let botonSubmit = document.querySelector(".botons")
+    let botonSubmit = document.querySelector("#botons")
    
     formulario.addEventListener("submit", function(e){
         ulErrores.innerHTML = ""
 
                let errores = []
 
-               let campoEmail = document.querySelector("input#email");
+               let campoEmail = document.querySelector("input#usuario");
                if (campoEmail.value == ""){
                    errores.push ("Ingrese su email completo")
                }
        
-               let campoPassword = document.querySelector("input#password");
+               let campoPassword = document.querySelector("input#contraseña");
                if (campoPassword.value == ""){
                    errores.push ("Su contraseña no debe estar vacia")
                } else if (campoPassword.value.length < 8){
                    errores.push ("Su contraseña debe tener mas de 8 caracteres")
                }
-
-                let campobox = document.querySelector('input#box')
-                if (campobox.value = ""){
-                    ulErrores.push("Rellene")}
             
                     if (errores.length > 0){
                         e.preventDefault();
